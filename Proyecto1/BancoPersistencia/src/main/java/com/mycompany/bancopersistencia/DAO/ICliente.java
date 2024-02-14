b8dfc3c776f0f11bc41577afd349500e11f0bca9
Gabriel <140923456+SrGabriel1@users.noreply.github.com>
2024-02-14 11:09:58 -0700

    Agrego los DAO

---
+++ Proyecto1/BancoPersistencia/src/main/java/com/mycompany/bancopersistencia/DAO/ICliente.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.mycompany.bancopersistencia.DAO;
+
+/**
+ *
+ * @author LV322
+ */
+public interface ICliente {
+
+}
